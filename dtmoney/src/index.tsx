f32ea917f0692c8d255287b994b92a2d7f17812a
Giovany Inocêncio <ino.giovany@gmail.com>
2022-08-15 21:20:25 -0300

    Adaptando o index.tsx para o react 18

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; 
+import {createRoot} from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
 
-ReactDOM.render(
+    this.get('/transactions', () =>{
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+
+  }
+})
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const rootElement = document.getElementById('root')!;
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
